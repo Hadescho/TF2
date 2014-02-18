fe524d349afb219b74f594040be4a995d08a62fa
Hadescho <ystata@abv.bg>
2014-02-18 14:57:13 +0200

    Commit with servlet-mapping problems.

--- src/org/elsys/TF2Checker/MainServer.java
+++ src/org/elsys/TF2Checker/MainServer.java
@@ -3,7 +3,12 @@ package org.elsys.TF2Checker;
 import java.io.IOException;
 import java.net.ServerSocket;
 
+import javax.net.ssl.HttpsURLConnection;
 import javax.print.attribute.standard.Severity;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import com.github.koraktor.steamcondenser.*;
 import com.github.koraktor.steamcondenser.exceptions.SteamCondenserException;
@@ -12,9 +17,12 @@ import com.github.koraktor.steamcondenser.steam.community.SteamId;
 
 
 public class MainServer {
-	public static void home () throws IOException, SteamCondenserException {
+	@GET
+	@Path("/userSearch")
+	@Produces(MediaType.APPLICATION_JSON)
+	public static SteamId GetUserInfo () throws IOException, SteamCondenserException {
 
-		SteamId player = SteamId.create("Hadescho");
+		return SteamId.create("Hadescho");
 		
 		
 	}
